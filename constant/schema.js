ab79dbd67b8bca8384d3c43735518ceccda7dd4f
atharva-agrawal <agrawal.atharva1999@gmail.com>
2020-10-26 15:11:51 +0530

    Enter

--- constant/schema.js
+++ constant/schema.js
@@ -1,4 +1,6 @@
 module.exports = {
   USER_SIGNUP: "/schema/user-signup.json",
   USER_LOGIN: "/schema/user-login.json",
+  MERCHANT_LOGIN: "/schema/mer-login.json",
+  MERCHANT_SIGNUP: "/schema/mer-signup.json",
 };
